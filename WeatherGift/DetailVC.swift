b125bbe75fb5998f8ad2bee4830bbf07a67e6e06
Mr. Michael Kelley <mx@kell4.com>
2019-02-27 13:22:01 -0500

    7.2 Complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Michael X Kelley on 2/27/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+   
+}
---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,19 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Michael X Kelley on 2/27/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
