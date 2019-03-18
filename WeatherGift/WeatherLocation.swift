b08e8994be9169b8afc9f9eb26d25943d6c4b384
Mr. Michael Kelley <mx@kell4.com>
2019-03-18 10:13:13 -0400

    7.10 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Michael X Kelley on 3/18/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
