6a6d27eb6e08cc715558fb97d6d7de79fc196208
Mr. Michael Kelley <mx@kell4.com>
2019-03-18 11:38:56 -0400

    7.15 Complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Michael X Kelley on 3/18/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "8c00b0b21d2b9fb33955c577582b45f8" + "/"
