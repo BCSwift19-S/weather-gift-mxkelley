8a3a7ed8c49e831502c15906717bd0c1d0be4c3b
Mr. Michael Kelley <mx@kell4.com>
2019-03-18 12:35:31 -0400

    7.16 Complete (with Challenge Problem)

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -8,16 +8,41 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class WeatherLocation {
     var name = ""
     var coordinates = ""
+    var currentTemp = "--"
+    var currentSummary = ""
     
-    func getWeather() {
+    func getWeather(completed: @escaping () -> ()) {
         
         let weatherURL = urlBase + urlAPIKey + coordinates
-        print(weatherURL)
+//        print(weatherURL)
         
-        Alamofire.request(weatherURL).responseJSON {response in print(response)}
+        Alamofire.request(weatherURL).responseJSON {response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+//                print("***** JSON: \(json)")
+                if let temperature = json["currently"]["temperature"].double {
+//                    print("***** Temperature inside getWeather = \(temperature)")
+                    let roundedTemp = String(format: "%3.f", temperature)
+                    self.currentTemp = roundedTemp + "°"
+                } else {
+                    print("Could not return a temperature")
+                }
+                if let summary = json["daily"]["summary"].string {
+                    self.currentSummary = summary
+//                    print("The summary for \(self.name) is \(self.currentSummary)")
+                } else {
+                    print("Could not return a summary")
+                }
+            case .failure(let error):
+                print(error)
+            }
+            completed()
+        }
     }
 }
