bdd139a4658a1f19d17c08e2702d11d8bc6b86a4
Mr. Michael Kelley <mx@kell4.com>
2019-03-24 12:36:23 -0400

    7.25 Complete

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -2,113 +2,18 @@
 //  WeatherLocation.swift
 //  WeatherGift
 //
-//  Created by Michael X Kelley on 3/18/19.
+//  Created by Michael X Kelley on 3/24/19.
 //  Copyright © 2019 Michael X Kelley. All rights reserved.
 //
 
 import Foundation
-import Alamofire
-import SwiftyJSON
 
-class WeatherLocation {
-    
-    struct DailyForecast {
-        var dailyMaxTemp: Double
-        var dailyMinTemp: Double
-        var dailyDate: Double
-        var dailySummary: String
-        var dailyIcon: String
-    }
-    
-    struct HourlyForecast {
-        var hourlyTime: Double
-        var hourlyTemperature: Double
-        var hourlyPrecipProb: Double
-        var hourlyIcon: String
-    }
-    
+class WeatherLocation: Codable {
     var name = ""
     var coordinates = ""
-    var currentTemp = "--"
-    var currentSummary = ""
-    var currentIcon = ""
-    var currentTime = 0.0
-    var timeZone = ""
-    var dailyForecastArray = [DailyForecast]()
-    var hourlyForecastArray = [HourlyForecast]()
     
-    func getWeather(completed: @escaping () -> ()) {
-        
-        let weatherURL = urlBase + urlAPIKey + coordinates
-//        print(weatherURL)
-        
-        Alamofire.request(weatherURL).responseJSON {response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-//                print("***** JSON: \(json)")
-                if let temperature = json["currently"]["temperature"].double {
-//                    print("***** Temperature inside getWeather = \(temperature)")
-                    let roundedTemp = String(format: "%3.f", temperature)
-                    self.currentTemp = roundedTemp + "°"
-                } else {
-                    print("Could not return a temperature")
-                }
-                if let summary = json["daily"]["summary"].string {
-                    self.currentSummary = summary
-//                    print("The summary for \(self.name) is \(self.currentSummary)")
-                } else {
-                    print("Could not return a summary")
-                }
-                if let icon = json["currently"]["icon"].string {
-                    self.currentIcon = icon
-                } else {
-                    print("Could not return an icon")
-                }
-                if let timeZone = json["timezone"].string {
-//                    print("TIMEZONE for \(self.name) is \(timeZone)")
-                    self.timeZone = timeZone
-                } else {
-                    print("Could not return a timeZone")
-                }
-                if let time = json["currently"]["time"].double {
-//                    print("TIME for \(self.name) is \(time)")
-                    self.currentTime = time
-                }
-                
-                let dailyDataArray = json["daily"]["data"]
-//                print("**** The dailyData array is: \(dailyDataArray)")
-                self.dailyForecastArray = []
-                let days = min(7, dailyDataArray.count-1)
-                for day in 1...days {
-                    let maxTemp = json["daily"]["data"][day]["temperatureHigh"].doubleValue
-                    let minTemp = json["daily"]["data"][day]["temperatureLow"].doubleValue
-                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
-                    let icon = json["daily"]["data"][day]["icon"].stringValue
-                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
-                    let newDailyForecast = DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailyDate: dateValue, dailySummary: dailySummary, dailyIcon: icon)
-                    self.dailyForecastArray.append(newDailyForecast)
-//                    print("***** newDailyForecast = \(newDailyForecast)")
-                }
-                
-                let hourlyDataArray = json["hourly"]["data"]
-                self.hourlyForecastArray = []
-                let hours = min(24, hourlyDataArray.count-1)
-                for hour in 1...hours {
-                    let hourlyTime = json["hourly"]["data"][hour]["time"].doubleValue
-                    let hourlyTemperature = json["hourly"]["data"][hour]["temperature"].doubleValue
-                    let hourlyPrecipProb = json["hourly"]["data"][hour]["precipProbability"].doubleValue
-                    let hourlyIcon = json["hourly"]["data"][hour]["icon"].stringValue
-                    let newHourlyForecast = HourlyForecast(hourlyTime: hourlyTime, hourlyTemperature: hourlyTemperature, hourlyPrecipProb: hourlyPrecipProb, hourlyIcon: hourlyIcon)
-                    self.hourlyForecastArray.append(newHourlyForecast)
-                }
-//                print("***** \(self.hourlyForecastArray)")
-                
-                
-            case .failure(let error):
-                print(error)
-            }
-            completed()
-        }
+    init(name: String, coordinates: String) {
+        self.name = name
+        self.coordinates = coordinates
     }
 }
